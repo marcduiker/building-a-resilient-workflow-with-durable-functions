42ba2e116142ac7d5a8d227be093b926b6e1f3d5
Marc Duiker <marcduiker@gmail.com>
2019-09-16 17:19:19 +0200

    Added function to call the estimate kinetic energy endpoint

---
+++ src/Demo.NEO.EventProcessing/Activities/EstimateKineticEnergyActivity.cs
@@ -0,0 +1,33 @@
+﻿using Demo.Neo.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Demo.NEO.EventProcessing.Activities
+{
+    public class EstimateKineticEnergyActivity
+    {
+        private readonly HttpClient client;
+
+        public EstimateKineticEnergyActivity(IHttpClientFactory httpClientFactory)
+        {
+            client = httpClientFactory.CreateClient();
+        }
+
+        [FunctionName(nameof(EstimateKineticEnergyActivity))]
+        public async Task<KineticEnergyResult> Run(
+          [ActivityTrigger] DetectedNeoEvent neoEvent,
+          ILogger logger)
+        {
+            var kineticEnergyEndpoint = new Uri(Environment.GetEnvironmentVariable("KineticEnergyEndpoint"));
+            var apiManagementKey = Environment.GetEnvironmentVariable("ApiManagementKey");
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", apiManagementKey);
+            var response = await client.PostAsJsonAsync(kineticEnergyEndpoint, neoEvent);
+            var result = await response.Content.ReadAsAsync<KineticEnergyResult>();
+
+            return result;
+        }
+    }
+}
