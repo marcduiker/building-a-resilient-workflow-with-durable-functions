1d9181a8e081f68a0ee0eab93644e04251910420
Marc Duiker <marcduiker@gmail.com>
2019-09-17 11:49:19 +0200

    Moved Generator sln file, updated timer trigger to generate a random nr of event each time.

--- src/Demo.NEO.Generator/EventGeneratorTimerTrigger.cs
+++ src/Demo.NEO.Generator/EventGeneratorTimerTrigger.cs
@@ -12,11 +12,13 @@ namespace Demos.Neo.Generator
     {
         [FunctionName(nameof(EventGeneratorTimerTrigger))]
         public async Task Run(
-            [TimerTrigger("*/10 * * * * *")] TimerInfo myTimer,
+            [TimerTrigger("*/15 * * * * *")] TimerInfo myTimer,
             [ServiceBus("neo-events", Connection = "GeneratorConnection")]IAsyncCollector<DetectedNeoEvent> collector,
             ILogger log)
         {
-            var nrOfEventsToGenerate = Convert.ToInt32(Environment.GetEnvironmentVariable("NumberOfEventsToGenerate"));
+            var maxNrOfEventsToGenerate = Convert.ToInt32(Environment.GetEnvironmentVariable("MaxNumberOfEventsToGenerate"));
+            
+            var nrOfEventsToGenerate = new Random().Next(1, maxNrOfEventsToGenerate);
             
             var addToCollectorTasks = NeoEventGenerator.Generate(nrOfEventsToGenerate)
                 .Select(neo => collector.AddAsync(neo));
