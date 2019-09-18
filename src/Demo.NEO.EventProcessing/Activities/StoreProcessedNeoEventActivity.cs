c97f5dc3a8e153ce235e3ac6670d7c884c4b06f2
Marc Duiker <marcduiker@gmail.com>
2019-09-18 14:24:42 +0200

    Updated models + implemented Storage of processed events

---
+++ src/Demo.NEO.EventProcessing/Activities/StoreProcessedNeoEventActivity.cs
@@ -0,0 +1,28 @@
+﻿using Demo.Neo.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Demo.NEO.EventProcessing.Activities
+{
+    public class StoreProcessedNeoEventActivity
+    {
+        [FunctionName(nameof(StoreProcessedNeoEventActivity))]
+        public async Task Run(
+          [ActivityTrigger] ProcessedNeoEvent processedNeoEvent,
+          IBinder binder,
+          ILogger logger)
+        {
+            var blobPath = $"neo/processed/{processedNeoEvent.Date.ToString("YYYYMMdd")}/{processedNeoEvent.TorinoImpact}/{processedNeoEvent.Id}";
+            var dynamicBlobBinding = new BlobAttribute(blobPath: blobPath);
+            var dynamicStorageBinding = new StorageAccountAttribute("ProcessedNeoStorage");
+
+            using (var writer = binder.Bind<TextWriter>(dynamicBlobBinding))
+            {
+                await writer.WriteAsync(JsonConvert.SerializeObject(processedNeoEvent));
+            }
+        }
+    }
+}
