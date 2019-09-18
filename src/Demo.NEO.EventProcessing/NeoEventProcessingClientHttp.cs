7da575a326f8ee0ebf3bb150764ba86c22a30238
Marc Duiker <marcduiker@gmail.com>
2019-09-18 17:22:14 +0200

    Added HttpTrigger client

---
+++ src/Demo.NEO.EventProcessing/NeoEventProcessingClientHttp.cs
@@ -0,0 +1,28 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Demo.Neo.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Demo.NEO.EventProcessing
+{
+    public class NeoEventProcessingClientHttp
+    {
+        [FunctionName(nameof(NeoEventProcessingClientHttp))]
+        public async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "POST", Route = "start")]HttpRequestMessage message,
+            [OrchestrationClient]DurableOrchestrationClientBase orchestrationClient,
+            ILogger log)
+        {
+            var detectedNeoEvent = await message.Content.ReadAsAsync<DetectedNeoEvent>();
+            var instanceId = await orchestrationClient.StartNewAsync(nameof(NeoEventProcessingOrchestration),
+                detectedNeoEvent);
+
+            log.LogInformation($"HTTP started orchestration with ID {instanceId}.");
+
+            return orchestrationClient.CreateCheckStatusResponse(message, instanceId);
+        }
+    }
+}
