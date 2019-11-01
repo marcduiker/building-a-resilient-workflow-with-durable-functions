f6b9a47e34e3d045e75fa4d99addbaa62a99a109
Marc Duiker <marcduiker@gmail.com>
2019-11-01 19:07:51 +0100

    Version increment to 1.0.4

--- src/Demo.NEO.Models/obj/Debug/netstandard2.0/Demo.NEO.Models.AssemblyInfo.cs
+++ src/Demo.NEO.Models/obj/Debug/netstandard2.0/Demo.NEO.Models.AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo.NEO.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.4.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo.NEO.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo.NEO.Models")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.3.0")]
